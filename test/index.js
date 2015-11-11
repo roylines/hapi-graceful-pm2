49a2ec6787092adc4571cf25f88e5f8c3140837d
roylines <roy@roylines.co.uk>
2015-11-11 12:39:07 +0000

    set node version

---
+++ test/index.js
@@ -0,0 +1,9 @@
+var Lab = require('lab');
+
+var lab = exports.lab = Lab.script();
+
+lab.experiment('hapi-graceful-pm2', function () {
+  lab.test('should', function (done) {
+    done();
+  });
+});
