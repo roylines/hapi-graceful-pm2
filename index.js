1fe918430be46f79bba37a010502394bdc175b33
Henrik Ruscon <henrik.dahlheim@gmail.com>
2018-01-03 20:27:27 +0100

    use async/await instead of promises

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const register = (server, options) => {
-    process.on('SIGINT', () => {
+    process.on('SIGINT', async () => {
         server.log(['info', 'pm2', 'shutdown'], 'stopping hapi...');
-        server.stop(options, () => {
-            server.log(['info', 'pm2', 'shutdown'], 'hapi stopped');
 
-            return process.exit(0);
-        });
+        await server.stop(options);
+
+        server.log(['info', 'pm2', 'shutdown'], 'hapi stopped');
+        return process.exit(0);
     });
 };
 
