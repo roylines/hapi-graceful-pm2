0e7ced61f994746a079d8daf814845031061fa75
Jeff Baldwin <jeffbaldwinjr@gmail.com>
2018-06-27 15:49:11 -0600

    Crude update to be Hapi V17 compatible.

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-exports.register = function(server, options, done) {
-  process.on('SIGINT', function() {
-    server.log(['info', 'pm2', 'shutdown'], 'stopping hapi...');
-    server.root.stop(options, function() {
-      server.log(['info', 'pm2', 'shutdown'], 'hapi stopped');
-      return process.exit(0);
-    });
-  });
+exports.plugin = {
+    register: function(server, options) {
+        process.on('SIGINT', function () {
+            server.log(['info', 'pm2', 'shutdown'], 'stopping hapi...');
+            server.root.stop(options, function () {
+                server.log(['info', 'pm2', 'shutdown'], 'hapi stopped');
+                return process.exit(0);
+            });
+        });
 
-  return done();
-};
-
-exports.register.attributes = {
-  pkg: require('./package.json')
+    },
+    pkg: require('./package.json')
 };
